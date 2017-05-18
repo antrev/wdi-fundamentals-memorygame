21278487cd4057a1a882ba674f69cd4b02b202f2
antrev <anton.revich@gmail.com>
2017-05-17 23:02:46 -0400

    created game logic

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var checkForMatch = function(){
+	
+	if (cardsInPlay[0] === cardsInPlay[2]){
+		alert("You found a match");
+	} else {
+		alert("Sorry try again")
+	}
+}
+
+var flipCard = function (cardId){
+	checkForMatch();
+	console.log("You flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
+
